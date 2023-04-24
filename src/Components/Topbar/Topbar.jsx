7aa15211a620f707f8ca06c1b607126976bd1d29
justina177 <justinaenenche@yahoo.com>
2023-04-24 17:50:05 +0100

    Added more icons and css to Topbar Component

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Topbar.css';
-import { NotificationsNone } from '@mui/icons-material';
+import { Language, NotificationsNone, Settings } from '@mui/icons-material';
+import image1 from '../../images/pexels-pixabay-38554-removebg-preview.png';
 
 export default function Topbar() {
   return (
@@ -12,7 +13,16 @@ export default function Topbar() {
             <div className="topbarRight">
                 <div className="topbarIconsContainer">
                     <NotificationsNone />
+                    <span className="topIconBadge">2</span>
                 </div>
+                <div className="topbarIconsContainer">
+                    <Language />
+                    <span className="topIconBadge">2</span>
+                </div>
+                <div className="topbarIconsContainer">
+                    <Settings />
+                </div>
+                <img src={image1} alt="" className="topAvater" />
             </div>
         </div>
     </div>
