1409cf688d5cfd0706410be30890c0c873aa13c0
justina177 <justinaenenche@yahoo.com>
2023-04-28 14:02:19 +0100

    implemented react router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Topbar from './Components/Topbar/Topbar';
 import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserList from './pages/userList/UserList';
 
 function App() {
   return (
-    <div className="App">
-      <Topbar />
-      <div className="container" >
-      <Sidebar />
-      <Home />
-      <div className="other">other pages</div>
-      </div> 
-    </div>
+    <Router>    
+        <Topbar />
+        <div className="container" >
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          
+          <Route path="/users" element={<UserList /> } />
+          
+        </Routes>
+        </div>   
+    </Router>
   );
 }
 
---
+++ src/pages/userList/UserList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './UserList.css';
+
+export default function UserList() {
+  return (
+    <div className="userList">
+        UserList
+
+    </div>
+  )
+}
