a2a38c595601669d6694464c5d17f01f8856c7b9
justina177 <justinaenenche@yahoo.com>
2023-05-01 05:05:57 +0100

    Added userEdit Widget and its css

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './User.css';
 import img1 from '../../images/pexels-pixabay-38554-removebg-preview.png';
-import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid } from '@mui/icons-material';
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from '@mui/icons-material';
 
 export default function user() {
   return (
@@ -25,6 +25,7 @@ export default function user() {
                 <PermIdentity className="userShowIcon" />
                 <span className="userShowInfoTitle">HannahB123</span>
             </div>   
+            
             <div className="userShowInfo">
                 <CalendarToday className="userShowIcon" />
                 <span className="userShowInfoTitle">10.12.1992</span>
@@ -38,7 +39,7 @@ export default function user() {
 
             <div className="userShowInfo">
                 <MailOutline className="userShowIcon" />
-                <span className="userShowInfoTitle">HannahB123@gmail.com</span>
+                <span className="userShowInfoTitle">hannahB123@gmail.com</span>
             </div>       
 
             <div className="userShowInfo">
@@ -46,26 +47,65 @@ export default function user() {
                 <span className="userShowInfoTitle">Abuja | Nigeria</span>
             </div>       
 
-            {/* <div className="userShowInfo">
-                <PermIdentity className="userShowIcon" />
-                <span className="userShowInfoTitle">HannahB123</span>
-            </div>       
+        </div>
+        </div>
+        <div className="userUpdate">
+            <span className="userUpdateTitle">Edit</span>
+            <form className="userUpdateForm">
+                <div className="userUpdateLeft">
+                    <div className="userUpdateItem">
+                        <label>Username</label>
+                        <input type="text" 
+                        placeholder='hannahb123'
+                        className='userUpdateInput'
+                         />
+                    </div>
 
-            <div className="userShowInfo">
-                <PermIdentity className="userShowIcon" />
-                <span className="userShowInfoTitle">HannahB123</span>
-            </div>              */}
-        </div>
-        </div>
-        <div className="userUpdate"></div>
+                    <div className="userUpdateItem">
+                        <label>Full Name</label>
+                        <input type="text" 
+                        placeholder='Hannah Benjamin'
+                        className='userUpdateInput'
+                         />
+                    </div>
 
+                    <div className="userUpdateItem">
+                        <label>Email</label>
+                        <input type="text" 
+                        placeholder='hannahB123@gmail.com'
+                        className='userUpdateInput'
+                         />
+                    </div>
+
+                    <div className="userUpdateItem">
+                        <label>Phone</label>
+                        <input type="text" 
+                        placeholder='+234 1234 5678'
+                        className='userUpdateInput'
+                         />
+                    </div>
+
+                    <div className="userUpdateItem">
+                        <label>Address</label>
+                        <input type="text" 
+                        placeholder='Abuja | Nigeria'
+                        className='userUpdateInput'
+                         />
+                    </div>
+                </div>
+                <div className="userUpdateRight">
+                    <div className="userUpdateUpload">
+                        <img className="userUpdateImg" src={img1} alt="" />
+                        <label htmlFor="file">
+                            <Publish className="userUpdateIcon" />
+                        </label>
+                        <input type='file' id='file' style={{ display: "none" }} />
+                    </div>
+                    <button className="userUpdateButton">Update</button>
+                </div>
+            </form>
         </div>
-        {/*    
-                
-                
-            </div> 
         </div>
-         */}
     </div>
   )
 }
